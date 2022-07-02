c46547fc5862bd6b1e90c06833137663601201ab
Mrlucos2022 <104990716+Mrlucos2022@users.noreply.github.com>
2022-07-01 18:07:50 -0700

    First Assignment on JavaScript table html
    My project on JavaScript table Hrml

---
+++ table.js
@@ -0,0 +1,104 @@
+let workers = [1];
+    worker = ["Thompson"];
+    worke = ["Anthony"];
+    work = [32];
+    wor = ["Male"];
+    wo = ["Head of Department"];
+    w = ["17/07/2014"];
+document.getElementById("a1").innerHTML = workers;
+document.getElementById("a2").innerHTML = worker;
+document.getElementById("a3").innerHTML = worke;
+document.getElementById("a4").innerHTML = work;
+document.getElementById("a5").innerHTML = wor;
+document.getElementById("a6").innerHTML = wo;
+document.getElementById("a7").innerHTML = w;
+
+
+let johnson = [2,"Johnson", "Mary", 23, "Female", "Software Engineer", "05/03/2019"];
+document.getElementById("b1").innerHTML = johnson[0];
+document.getElementById("b2").innerHTML = johnson[1];
+document.getElementById("b3").innerHTML = johnson[2];
+document.getElementById("b4").innerHTML = johnson[3];
+document.getElementById("b5").innerHTML = johnson[4];
+document.getElementById("b6").innerHTML = johnson[5];
+document.getElementById("b7").innerHTML = johnson[6];
+
+
+let manager = [3, "Chisom", "Onyeka", 28, "Female", "Human Resources Manager", "21/06/2016"];
+document.getElementById("c1").innerHTML = manager[0];
+document.getElementById("c2").innerHTML = manager[1];
+document.getElementById("c3").innerHTML = manager[2];
+document.getElementById("c4").innerHTML = manager[3];
+document.getElementById("c5").innerHTML = manager[4];
+document.getElementById("c6").innerHTML = manager[5];
+document.getElementById("c7").innerHTML = manager[6];
+
+
+let olabisi = [4, "Jide", "Olabisi", 29, "Female", "Assistant Head (Software Department)", "05/06/2016"];
+document.getElementById("d1").innerHTML = olabisi[0];
+document.getElementById("d2").innerHTML = olabisi[1];
+document.getElementById("d3").innerHTML = olabisi[2];
+document.getElementById("d4").innerHTML = olabisi[3];
+document.getElementById("d5").innerHTML = olabisi[4];
+document.getElementById("d6").innerHTML = olabisi[5];
+document.getElementById("d7").innerHTML = olabisi[6];
+
+
+let teacher = [5, "Williams", "Ajayi", 30, "Male", "HeadofSoftwareDepartment", "06/09/2015"];
+document.getElementById("e1").innerHTML = teacher[0];
+document.getElementById("e2").innerHTML = teacher[1];
+document.getElementById("e3").innerHTML = teacher[2];
+document.getElementById("e4").innerHTML = teacher[3];
+document.getElementById("e5").innerHTML = teacher[4];
+document.getElementById("e6").innerHTML = teacher[5];
+document.getElementById("e7").innerHTML = teacher[6];
+
+
+let adeyemi = [6, "Naney", "Chukwuemeka", 27, "Female", "Secretary", "23/08/2016"];
+document.getElementById("f1").innerHTML = adeyemi[0];
+document.getElementById("f2").innerHTML = adeyemi[1];
+document.getElementById("f3").innerHTML = adeyemi[2];
+document.getElementById("f4").innerHTML = adeyemi[3];
+document.getElementById("f5").innerHTML = adeyemi[4];
+document.getElementById("f6").innerHTML = adeyemi[5];
+document.getElementById("f7").innerHTML = adeyemi[6];
+
+
+let uchemba = [7, "Nicholas", "Uchemba", 26, "Male", "Software Engineer", "22/02/2020"];
+document.getElementById("g1").innerHTML = uchemba[0];
+document.getElementById("g2").innerHTML = uchemba[1];
+document.getElementById("g3").innerHTML = uchemba[2];
+document.getElementById("g4").innerHTML = uchemba[3];
+document.getElementById("g5").innerHTML = uchemba[4];
+document.getElementById("g6").innerHTML = uchemba[5];
+document.getElementById("g7").innerHTML = uchemba[6];
+
+
+let graphic = [8, "Ahmed", "Olayinka", 24, "Male", "GraphicDesigner", "15/08/2020"];
+document.getElementById("h1").innerHTML = graphic[0];
+document.getElementById("h2").innerHTML = graphic[1];
+document.getElementById("h3").innerHTML = graphic[2];
+document.getElementById("h4").innerHTML = graphic[3];
+document.getElementById("h5").innerHTML = graphic[4];
+document.getElementById("h6").innerHTML = graphic[5];
+document.getElementById("h7").innerHTML = graphic[6];
+
+
+let finance = [9, "Halima", "Mohammed", 26, "Female", "Senior Finance Manager", "30/05/2018"];
+document.getElementById("i1").innerHTML = finance[0];
+document.getElementById("i2").innerHTML = finance[1];
+document.getElementById("i3").innerHTML = finance[2];
+document.getElementById("i4").innerHTML = finance[3];
+document.getElementById("i5").innerHTML = finance[4];
+document.getElementById("i6").innerHTML = finance[5];
+document.getElementById("i7").innerHTML = finance[6];
+
+
+let richard = [10, "Richard", "Adeyemi", 41, "Male", "CEO", "17/11/2021"];
+document.getElementById("j1").innerHTML = richard[0];
+document.getElementById("j2").innerHTML = richard[1];
+document.getElementById("j3").innerHTML = richard[2];
+document.getElementById("j4").innerHTML = richard[3];
+document.getElementById("j5").innerHTML = richard[4];
+document.getElementById("j6").innerHTML = richard[5];
+document.getElementById("j7").innerHTML = richard[6];
